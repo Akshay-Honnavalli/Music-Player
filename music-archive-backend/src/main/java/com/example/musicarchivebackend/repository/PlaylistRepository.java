9fd468dccec709a25495a3c1a55da957871dc9f8
AKSHAY PRASAD HONNAVALLI <93462091+Akshay-Honnavalli@users.noreply.github.com>
2024-04-08 23:39:34 +0530

    Delete music-archive-backend directory

--- music-archive-backend/src/main/java/com/example/musicarchivebackend/repository/PlaylistRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.musicarchivebackend.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.musicarchivebackend.model.Playlist;
-
-public interface PlaylistRepository extends MongoRepository<Playlist, String> {
-    // Custom query methods can be defined here if needed
-}
